7638c281884774f3a4aee44f83a29696e962f01d
fuqiang04 <fuqiang04@zuoyebang.com>
2020-12-30 16:57:10 +0800

    ArrayField

--- lib/fields/ArrayField.tsx
+++ lib/fields/ArrayField.tsx
@@ -1,5 +1,5 @@
 import { defineComponent } from "vue";
-import { FieldPropsDefine } from "../types";
+import { FieldPropsDefine, Schema } from "../types";
 import { useVJSFContext } from "../context";
 /** 三种情况
  * {
@@ -25,8 +25,30 @@ export default defineComponent({
   props: FieldPropsDefine,
   setup(props) {
     const context = useVJSFContext();
+    const handleMultiTypeChange = (v: any, index: number) => {
+      const { value } = props;
+      const arr = Array.isArray(value) ? value : [];
+      arr[index] = v;
+      props.onChange(arr);
+    };
     return () => {
+      const { schema, rootSchema, value } = props;
       const SchemaItem = context.SchemaItem;
+      const isMultiType = Array.isArray(schema.items);
+
+      if (isMultiType) {
+        const items: Schema[] = schema.items as any;
+        const arr = Array.isArray(value) ? value : [];
+        return items.map((s: Schema, index: number) => (
+          <SchemaItem
+            schema={s}
+            key={index}
+            rootSchema={rootSchema}
+            value={arr[index]}
+            onChange={(v: any) => handleMultiTypeChange(v, index)}
+          />
+        ));
+      }
     };
   }
 });
